8c625c4d43f774c476bf56f7a60d22f6138adc71
patterenuslogic <34865449+patterenuslogic@users.noreply.github.com>
2017-12-26 08:56:32 -0800

    Update InAppBrowser.java

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1119,5 +1119,17 @@ public class InAppBrowser extends CordovaPlugin {
             // By default handle 401 like we'd normally do!
             super.onReceivedHttpAuthRequest(view, handler, host, realm);
         }
+	    
+@Override
+public boolean shouldOverrideUrlLoading(WebView view, String url) {
+    if( url.startsWith("http:") || url.startsWith("https:") ) {
+        return false;
+    }
+
+    // Otherwise allow the OS to handle it
+    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+    view.getContext().startActivity( intent );
+    return true;
+}
     }
 }
